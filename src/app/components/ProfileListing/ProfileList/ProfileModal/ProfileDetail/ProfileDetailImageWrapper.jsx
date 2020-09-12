75f965dca1774a31c1705fa358eade863667b3ae
james-leung <jlfly12@gmail.com>
2020-09-12 08:49:21 +0100

    Search bar update and birth date formatting

--- src/app/components/ProfileListing/ProfileList/ProfileModal/ProfileDetail/ProfileDetailImageWrapper.jsx
+++ src/app/components/ProfileListing/ProfileList/ProfileModal/ProfileDetail/ProfileDetailImageWrapper.jsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export default styled.img`
-  box-sizing: border-box;
   margin: 20px;
   border-radius: 100%;
   border: 5px solid grey;
-  box-shadow: inset 5px inset;
+  box-shadow: 0 0 5px grey inset;
 `;
